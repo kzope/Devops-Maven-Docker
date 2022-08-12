59deefb84e9cd70ed15f30754d3907bc51246742
kzope <99174207+kzope@users.noreply.github.com>
2022-08-12 14:21:45 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Techlanders Solutions</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1> Hi, I am Kunal. </h1>");
+		out.println("<h1> Hello, I am Kunal. </h1>");
 		out.println("</body>");
 		out.println("</html>");
 
